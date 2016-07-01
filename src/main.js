d8b5688459bec61909bf358227426d1fc773fff4
st1ny <stiny@stiny.org>
2016-06-30 21:34:51 -0700

    Adds room controller.

--- src/main.js
+++ src/main.js
@@ -2,21 +2,27 @@ module.exports.loop = function () {
     //clear memory
 
     for (let names in Memory.creeps) {
-        if (Game.creeps[names] == undefined) {
-            delete Memory.creeps[names];
+        if (Memory.creeps.hasOwnProperty(names)) {
+            if (Memory.creeps[names] == undefined) {
+                delete Memory.creeps[names];
+            }
         }
     }
 
     // Start off by iterating through all the rooms
     for (let room in Game.rooms) {
-        if (Game.rooms[room].controller.my) {
-            // This is our room so let's start upgrading
-        } else if (Game.rooms[room].controller.owner != undefined {
-            // This isn't ours and isn't blank?
-        } else if (Game.rooms[room].controller.owner == undefined) {
-            // This isn't ours and is blank
+        if (Game.rooms.hasOwnProperty(room)) {
+            var roomObj = Game.rooms[room];
+            if (roomObj.controller.my) {
+                // This is our room so let's start upgrading
+                var roomController = require('controller.Room');
+                roomController.run(roomObj);
+            } else if (roomObj.controller.owner != undefined) {
+                // This isn't ours and isn't blank?
+            } else if (roomObj.controller.owner == undefined) {
+                // This isn't ours and is blank
+            }
         }
     }
-
 };
 
