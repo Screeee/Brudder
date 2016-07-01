e0225fce7ed11f7361d6d8813e6964d763498c54
Lamontjas <jlamont@stu.norwich.edu>
2016-07-01 00:29:32 -0400

    Added harvester spawns

--- src/main.js
+++ src/main.js
@@ -9,9 +9,18 @@ module.exports.loop = function () {
 
     // Start off by iterating through all the rooms
     for (let room in Game.rooms) {
-        if (Game.rooms[room].controller.my) {
-            // This is our room so let's start upgrading
-        } else if (Game.rooms[room].controller.owner != undefined {
+        if (Game.rooms[room].controller.my)
+        {
+            if (Game.rooms[room].controller.level == 1) {
+                var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester' && c.room == Game.rooms[room]);
+                var Spawn1 =  Game.rooms[room].find(STRUCTURE_SPAWN);
+                if (numberOfHarvesters < 2) {
+                    Game.spawns.Spawn1.createCreep([WORK,MOVE,CARRY], undefined, {role:'harvester'});
+                }
+            }
+        }
+
+        else if (Game.rooms[room].controller.owner != undefined) {
             // This isn't ours and isn't blank?
         } else if (Game.rooms[room].controller.owner == undefined) {
             // This isn't ours and is blank
